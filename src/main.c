fc3126c5d39ebc5b34f0860fdf3eca085a5ebfa9
dylan <down1995@tutanota.com>
2024-05-27 10:53:20 +0800

    Added comments

--- src/main.c
+++ src/main.c
@@ -5,16 +5,19 @@ int main(int argc, char* argv[]){
   char buffer[1024];
   size_t bytes_read;
 
-  fp = fopen(argv[1],"rb");
+  // Open the file defined in the second argument
+  fp = fopen(argv[1],"rb"); 
   if (fp == NULL){
     perror("Error opening file");
     return 1;
   }
   
+  // Reads file to buffer string, prints buffer string.
   while ((bytes_read = fread(buffer, 1, 1024, fp)) > 0){
     printf("%.*s", (int)bytes_read, buffer);
   }
-
+  
+  // Closes the fp file pointer
   fclose(fp);
   return 0;
 }
